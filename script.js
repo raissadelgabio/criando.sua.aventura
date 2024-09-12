5fd971963201d3fa0a19697e7fe58b739f34b723
raissadelgabio <168777076+raissadelgabio@users.noreply.github.com>
2024-09-12 16:33:29 -0300

    projeto final

---
+++ script.js
@@ -0,0 +1,11 @@
+const avança = document.queryselectorA11 (btn-proximo)
+
+avança.forEach (button)
+button.addEventListener (click , funtion())
+    const atual= document.querySelector ('.ativo')
+    const proximoPasso ='passo-' + this.getAttribute('data-proximo');
+
+atual.classList.romove('ativo');
+document.getElementById (proximoPasso).ClassList.add('ativo');
+
+
